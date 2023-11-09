986fb7a750186d611f0213b1fd4931e807d80385
CodeSandbox <support@codesandbox.io>
2023-11-09 23:41:51 +0000

    Sandbox snapshot commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { InferGetServerSidePropsType } from "next";
 
 export default function Home({
   data,
-}: InferGetServerSidePropsType<GetServerSideProps>) {
+}: InferGetServerSidePropsType<typeof GetServerSideProps>) {
   return (
     <div className="snap-y snap-mandatory grid grid-cols-1 md:grid-cols-3 gap-2 justify-items-center md:items-start items-center w-auto">
       <ReviewCardList />
