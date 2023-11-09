c9c980576bd9908eb38a341b4df0f99ffa04f7a5
CodeSandbox <support@codesandbox.io>
2023-11-09 23:58:33 +0000

    Sandbox snapshot commit

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -5,7 +5,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
   await dbConnect();
   try {
-    const reviews = await ReviewModel.find({});
+    const reviews = await fetch("https://eu-central-1.aws.data.mongodb-api.com/app/reviewapp-xwles/endpoint/getAll",{
+      headers:{
+        "Content-Type":"application/json",
+        "Accept":"application/json",
+        "API-Key":process.env.
+      }
+    });
     res.status(200).json({ success: true, data: reviews });
   } catch (error) {
     res.status(400).json({ success: false });
